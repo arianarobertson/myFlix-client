db9758872c5d2fc912f8e7178885873641725499
arianarobertson <arobertson22@berkeley.edu>
2023-11-21 21:45:22 -0800

    updated movie cards

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -27181,30 +27181,40 @@ const MainView = ()=>{
         {
             id: 1,
             title: "Jurassic Park",
+            description: "A wealthy entrepreneur secretly creates a theme park featuring living dinosaurs drawn from prehistoric DNA.",
+            genre: "Action",
             image: "https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_FMjpg_UX1000_.jpg",
             director: "Steven Spielberg"
         },
         {
             id: 2,
             title: "Pulp Fiction",
+            description: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+            genre: "Drama",
             image: "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg",
             director: "Quentin Tarantino"
         },
         {
             id: 3,
             title: "Inception",
+            description: "A thief who enters the dreams of others to steal their secrets must plant an idea into the mind of a CEO.",
+            genre: "Science Fiction",
             image: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UX1000_.jpg",
             director: "Christopher Nolan"
         },
         {
             id: 4,
             title: "E.T. the Extra-Terrestrial",
+            description: "A troubled child summons the courage to help a friendly alien escape Earth and return to his home world.",
+            genre: "Action",
             image: "https://m.media-amazon.com/images/M/MV5BMTQ2ODFlMDAtNzdhOC00ZDYzLWE3YTMtNDU4ZGFmZmJmYTczXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg",
             director: "Steven Spielberg"
         },
         {
             id: 5,
             title: "Django Unchained",
+            description: "With the help of a German bounty hunter, a freed slave sets out to rescue his wife from a brutal Mississippi plantation owner.",
+            genre: "Drama",
             image: "https://m.media-amazon.com/images/M/MV5BMjIyNTQ5NjQ1OV5BMl5BanBnXkFtZTcwODg1MDU4OA@@._V1_FMjpg_UX1000_.jpg",
             director: "Quentin Tarantino"
         }
@@ -27244,7 +27254,7 @@ const MainView = ()=>{
         columnNumber: 9
     }, undefined);
 };
-_s(MainView, "tfDouyUVAGj7qCOoS1PYjh9SKD0=");
+_s(MainView, "RLRTC7mVEkbMb0X3RtI493/OOII=");
 _c = MainView;
 var _c;
 $RefreshReg$(_c, "MainView");
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,11 +4,11 @@ import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
     const [movies, setMovies] = useState([
-        { id: 1, title: "Jurassic Park", image: "https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_FMjpg_UX1000_.jpg", director: "Steven Spielberg" },
-        { id: 2, title: "Pulp Fiction", image: "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg", director: "Quentin Tarantino" },
-        { id: 3, title: "Inception", image: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UX1000_.jpg", director: "Christopher Nolan" },
-        { id: 4, title: "E.T. the Extra-Terrestrial", image: "https://m.media-amazon.com/images/M/MV5BMTQ2ODFlMDAtNzdhOC00ZDYzLWE3YTMtNDU4ZGFmZmJmYTczXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg", director: "Steven Spielberg" },
-        { id: 5, title: "Django Unchained", image: "https://m.media-amazon.com/images/M/MV5BMjIyNTQ5NjQ1OV5BMl5BanBnXkFtZTcwODg1MDU4OA@@._V1_FMjpg_UX1000_.jpg", director: "Quentin Tarantino" }
+        { id: 1, title: "Jurassic Park", description: "A wealthy entrepreneur secretly creates a theme park featuring living dinosaurs drawn from prehistoric DNA.", genre: "Action", image: "https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_FMjpg_UX1000_.jpg", director: "Steven Spielberg" },
+        { id: 2, title: "Pulp Fiction", description: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.", genre: "Drama", image: "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg", director: "Quentin Tarantino" },
+        { id: 3, title: "Inception", description: "A thief who enters the dreams of others to steal their secrets must plant an idea into the mind of a CEO.", genre: "Science Fiction", image: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UX1000_.jpg", director: "Christopher Nolan" },
+        { id: 4, title: "E.T. the Extra-Terrestrial", description: "A troubled child summons the courage to help a friendly alien escape Earth and return to his home world.", genre: "Action", image: "https://m.media-amazon.com/images/M/MV5BMTQ2ODFlMDAtNzdhOC00ZDYzLWE3YTMtNDU4ZGFmZmJmYTczXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg", director: "Steven Spielberg" },
+        { id: 5, title: "Django Unchained", description: "With the help of a German bounty hunter, a freed slave sets out to rescue his wife from a brutal Mississippi plantation owner.", genre: "Drama", image: "https://m.media-amazon.com/images/M/MV5BMjIyNTQ5NjQ1OV5BMl5BanBnXkFtZTcwODg1MDU4OA@@._V1_FMjpg_UX1000_.jpg", director: "Quentin Tarantino" }
     ]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
